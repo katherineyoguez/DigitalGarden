ab1c76e373c448af5ac37b7311ebb412277485c0
Katherine Yoguez <katherinecarlotayoguez@gmail.com>
2021-08-26 21:36:20 -0400

    updated index.js second time

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,5 @@
 const User = require('./User');
 const Plants = require('./Plants')
-// const Stripe = require('./Stripe')
-// const Lists = require('./Lists')
+
 
 module.exports = { User, Plants };
