f8aece82f3711b1639b2f671665e5b23190bb15d
JoelBal <gmail>
2021-08-31 19:39:24 -0400

    stripe starter

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -161,4 +161,43 @@ const resolvers = {
     }
 };
 
+
+//Stripe Starter Code
+
+// var stripeHandler = StripeCheckout.configure({
+//   key: stripePublicKey,
+//   locale: 'en',
+//   token: function(token) {
+//       var items =[]
+//       var cartItemContainer = document.getElementsByClassName('cart-items')[0]
+//       var cartRows = cartItemContainer.getElementsByClassName('cart-row')
+//       for (var i = 0; i < cartRows.length; i++) {
+//           var cartRow =cartRows[i]
+//           var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+//           var quantity = quantityElement.value
+//           var id = cartRow.dataset.itemId
+//           items.push({
+//               id: id,
+//               quantity: quantity
+//           })
+//       }
+//       fetch('/purchase', {
+//           method: 'POST',
+//           headers: {
+//               'Content-Type': 'application/json',
+//               'Accept': 'application/json'
+//           },
+//           body: JSON.stringify({
+//               stripeTokenId: token.id,
+//               items: items
+//           })
+//       }).then(function(res) {
+//           return res.json()
+//       }).then(function(data) {
+//           alert(data.message)
+//       })
+//   }
+// })
+
+
 module.exports = resolvers;
